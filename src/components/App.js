0d553e5e16f904246710a12f8c9889969d86a013
Pawan kr <itsmekr.info@gmail.com>
2021-05-25 15:01:29 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,27 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  const [time, setTime] = useState(0);
 
+  useEffect(()=>{
+    setTimeout(()=>{
+      time > 0 && setTime (time-1);
+    },1000)
+  },[time]);
+
+  const fun = (e) =>{
+    if(e.key==='Enter'){
+      setTime(Math.floor(e.target.value));
+    }
+  }
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={fun /* callback function */} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{time/* remaining time */}</div>
     </div>
   )
 }
